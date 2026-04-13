633dcbfb410105a04f414e7d4b73d3a0b3d8041d
AnnadataHemanth <annadathahemanth578@gmail.com>
2026-04-14 01:20:50 +0530

    main.py is updated

--- app/main.py
+++ app/main.py
@@ -2,18 +2,21 @@ from fastapi import FastAPI
 from app.api.routes import router
 from database import init_db
 from starlette.middleware.sessions import SessionMiddleware
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = FastAPI(
     title="Receptionist AI",
     description="Voice-based AI assistant backend",
     version="0.1.0"
 )
-app.add_middleware(SessionMiddleware, secret_key="super-secret-key")
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 init_db()
 
 app.include_router(router)
 
 @app.get("/")
 def root():
-    return {"status": "Jarvis backend is running"}
+    return {"status": "Receptionist AI backend is running"}
